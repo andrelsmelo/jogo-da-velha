7a0d82871a83d371ad3c31b234b962f117b23708
André Melo <andrelucassmelo@gmail.com>
2022-05-02 10:22:14 -0300

    Jogo da velha v1

---
+++ scripts.js
@@ -0,0 +1,96 @@
+var divElement = document.querySelector('div'),
+	tableElement = document.querySelector('table');
+
+var Game = {
+	start(){
+		this.field = [
+			['','',''],
+			['','',''],
+			['','','']
+		];
+		this.currentPlayer = 'X';
+		this.isFinished = false;
+		this.round = 0;
+		this.render();
+	},
+
+    // Função que inverte jogadores
+
+	nextPlayer(){
+		this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+	},
+    // Função que seta rounds e renderiza jogada
+
+	setField(line, column){
+		if(!this.isFinished && this.field[line][column] === ''){
+			this.field[line][column] = this.currentPlayer;
+			this.nextPlayer();
+			this.round++;
+			this.render();
+		}
+	},
+    // Função que valida se jogo acabou e vencedor
+	isGameOver(){
+		var field = this.field,
+			rows = 3,
+			cols = 3,
+			totalRow = 0,
+			totalCol = 0;
+		for(var i = 0; i < rows; i++){
+			totalRow = 0;
+			totalCol = 0;
+			for(var j = 0; j < cols; j++){
+				if(field[i][j] === 'X'){
+					totalRow++;
+				}
+				if(field[i][j] === 'O'){
+					totalRow--;
+				}
+				if(field[j][i] === 'X'){
+					totalCol++;
+				}
+				if(field[j][i] === 'O'){
+					totalCol--;
+				}
+			}
+			if(totalRow === 3 || totalCol === 3){
+				return 'X';
+			}
+			if(totalRow === -3 || totalCol === -3){
+				return 'O';
+			}
+		}
+		if(field[0][0] !== '' && field[0][0] === field[1][1] && field[1][1] === field[2][2]){
+			return field[0][0];
+		}
+		if(field[0][2] !== '' && field[0][2] === field[1][1] && field[1][1] === field[2][0]){
+			return field[0][2];
+		}
+		if(this.round === rows * cols){
+			return 'Deu velha';
+		}
+
+	},
+
+    // Função de Renderização e visualização de infos no front (Vencedor, Jogador Atual)
+	render(){
+		var winner = this.isGameOver();
+		divElement.textContent = winner ? `Vencedor: ${winner}` : `Jogador atual: ${this.currentPlayer}`;
+		
+		if(winner){
+			this.isFinished = true;
+		}
+
+		var template = '';
+		this.field.forEach((line, lineIndex)=>{
+			template += '<tr>';
+			line.forEach((column, columnIndex)=>{
+				template += `<td onclick="Game.setField(${lineIndex}, ${columnIndex})" >${column}</td>`;
+			})
+			template += '</tr>'
+		})
+		tableElement.innerHTML = template;
+	}
+}
+
+Game.start();
